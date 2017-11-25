fdc0efc494f0be67e8c3f7d66058d37ca1514f57
Bram Vannevel <bram_vannevel2@hotmail.com>
2017-11-25 13:41:28 +0100

    removed backend so i can remove node_mudules

--- projectbackend/models/Message.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose');
-
-var MessageSchema = new mongoose.Schema({
-    text: String,
-    timestamp: Date,
-    user: String
-});
-
-mongoose.model('Message', MessageSchema);
