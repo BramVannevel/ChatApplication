6bfc32aac8daea64d31af48dbc8cf038ccd3f0bf
Bram Vannevel <bram_vannevel2@hotmail.com>
2017-12-01 18:44:40 +0100

    changes

--- src/app/chat/conversation-data.service.ts
+++ src/app/chat/conversation-data.service.ts
@@ -47,7 +47,7 @@ export class ConversationDataService {
         if(conv.users.find(user => user === userid)){
           if(this._conversations.getValue() !== conv._id){
             this._conversations.next(conv._id);
-            console.log(`CHANGING VALUE id:${conv._id} GETVALUE:${this._conversations.getValue()}`);
+            //console.log(`CHANGING VALUE id:${conv._id} GETVALUE:${this._conversations.getValue()}`);
           } 
         }
       }
@@ -56,7 +56,7 @@ export class ConversationDataService {
 
   changeConversationId(id){
     if(this._conversations.getValue() !== id){
-      console.log(`CHANGING VALUE id:${id} GETVALUE:${this._conversations.getValue()}`);
+      //console.log(`CHANGING VALUE id:${id} GETVALUE:${this._conversations.getValue()}`);
       this._conversations.next(id);
     }
   }
